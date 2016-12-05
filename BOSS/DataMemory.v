e1d853177fb99462e44cad6293394e9256ee4147
s10109670 <b03902075@ntu.edu.tw>
2016-12-06 02:06:25 +0800

    haha

---
+++ BOSS/DataMemory.v
@@ -0,0 +1,27 @@
+module DataMemory(
+	memRead_i,
+	memWrite_i,
+	ALUOut_i,
+	WriteData_i,
+	ReadData_o
+);
+
+input 			memRead_i;
+input			memWrite_i;
+input	[31:0]		ALUOut_i;
+input	[31:0]		WriteData_i;
+output	[31:0]		ReadData_o;
+
+reg	[31:0]		memory	[0:31];
+
+
+always@ (*)	
+begin
+	if(memWrite_i)
+		memory[ALUOut_i[15:0]] <= WriteData_i;
+	else if(memRead_i)
+		ReadData_o <= memory[ALUOut_i[15:0]];
+	else;
+end
+
+endmodule
