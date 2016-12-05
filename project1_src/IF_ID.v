d19feb7cd8e6de362dd579d6c4a1fc242a87241b
jihnenglin <b03902005@ntu.edu.tw>
2016-12-05 17:54:53 +0800

    fixed some bugs

--- project1_src/IF_ID.v
+++ project1_src/IF_ID.v
@@ -6,24 +6,26 @@ module IF_ID
 	IFIDWrite_i,
 	flush_i,
 	addr_o,
-	instr_o;
-}
+	instr_o
+);
 
-input	clk_i;
-input	[31:0]	addr_i, instr_i;
-input	IFIDWrite_i, flush_i;
-output	[31:0]	addr_o, instr_o;
+input			clk_i;
+input		[31:0]	addr_i, instr_i;
+input			IFIDWrite_i, flush_i;
+output reg	[31:0]	addr_o, instr_o;
 
 always@(negedge clk_i) begin
 	if(flush_i == 1) begin
 		addr_o <= addr_i;
 		instr_o <= 32'b0;
 	end
-	else if(IFIDWrite == 0) begin
+	else if(IFIDWrite_i == 0) begin
 		addr_o <= addr_i;
 		instr_o <= instr_i;
 	end
 	else begin
 		//stall
 	end
+end
+
 endmodule
