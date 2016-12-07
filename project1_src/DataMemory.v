3ae79fc5d13f7f60c6e94b94d9d7d0bb73586f65
s10109670 <s10109670@github.com>
2016-12-08 01:35:41 +0800

    require

--- project1_src/DataMemory.v
+++ project1_src/DataMemory.v
@@ -14,15 +14,15 @@ input	[31:0]		ALUOut_i;
 input	[31:0]		WriteData_i;
 output	[31:0]		ReadData_o;
 
-reg	[31:0]		memory	[0:31];
+reg	[7:0]		memory	[0:31];
 reg	[31:0]		ReadData_o;
 
 
 always@ (*)	
 begin
-	if(memWrite_i && ~clk_i)
+	if(memWrite_i && clk_i)
 		memory[ALUOut_i] <= WriteData_i;
-	else if(memRead_i && clk_i)
+	else if(memRead_i && ~clk_i)
 		ReadData_o <= memory[ALUOut_i];
 	else;
 end
