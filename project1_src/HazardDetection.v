e9a9fedb0571319f612245b3c1b2e1ca4ca065b8
jihnenglin <b03902005@ntu.edu.tw>
2016-12-07 23:13:33 +0800

    change implementation

--- project1_src/HazardDetection.v
+++ project1_src/HazardDetection.v
@@ -15,7 +15,7 @@ input		[4:0]	IDEX_RegisterRt_i;
 input		[31:0]	instr_i;
 output reg		PCWrite_o, IFIDWrite_o, MUX8_o;
 
-always@(negedge clk_i)begin
+/*always@(negedge clk_i)begin
 	if(IDEX_MemRead_i && 
 	   (IDEX_RegisterRt_i == instr_i[25:21] ||
             IDEX_RegisterRt_i == instr_i[20:16]))begin
@@ -24,17 +24,19 @@ always@(negedge clk_i)begin
 	else begin
 		MUX8_o <= 1'b0;
 	end
-end
+end*/
 always@(posedge clk_i)begin
 	if(IDEX_MemRead_i && 
 	   (IDEX_RegisterRt_i == instr_i[25:21] ||
             IDEX_RegisterRt_i == instr_i[20:16]))begin
 		PCWrite_o <= 1'b1;
 		IFIDWrite_o <= 1'b1;
+		MUX8_o <= 1'b1;
 	end
 	else begin
 		PCWrite_o <= 1'b0;
 		IFIDWrite_o <= 1'b0;
+		MUX8_o <= 1'b0;
 	end
 end
 
