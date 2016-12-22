6efa5b17375d07540952764d48d525b22da6eb5d
jihnenglin <b03902005@ntu.edu.tw>
2016-12-23 01:06:42 +0800

    add stall_i for if/id and id/ex

--- dcache_one_way_example/ID_EX.v
+++ dcache_one_way_example/ID_EX.v
@@ -11,6 +11,7 @@ module ID_EX
 	ctrl_WB_i,
 	ctrl_M_i,
 	ctrl_EX_i,
+	stall_i,
 	instr1115_o,
 	instr1620_MUX_o,
 	instr1620_FW_o,
@@ -31,6 +32,7 @@ input		[31:0]	sign_extend_i, RS_data_i, RT_data_i;
 input		[1:0]	ctrl_WB_i;
 input		[1:0]	ctrl_M_i;
 input		[3:0]	ctrl_EX_i;
+input			stall_i;
 output reg	[4:0]	instr1115_o, instr1620_MUX_o, instr1620_FW_o, instr2125_o;
 output reg	[31:0]	sign_extend_o, RS_data_o, RT_data_o;
 output reg	[1:0]	ctrl_WB_o;
@@ -39,18 +41,22 @@ output reg	[1:0]	ALUOp_o;
 output reg		ALUSrc_o, RegDst_o;
 
 always@(negedge clk_i) begin
-	instr1115_o <= instr1115_i;
-	instr1620_MUX_o <= instr1620_MUX_i;
-	instr1620_FW_o <= instr1620_FW_i;
-	instr2125_o <= instr2125_i;
-	sign_extend_o <= sign_extend_i;
-	RS_data_o <= RS_data_i;
-	RT_data_o <= RT_data_i;
-	ctrl_WB_o <= ctrl_WB_i;
-	ctrl_M_o <= ctrl_M_i;
-	RegDst_o <= ctrl_EX_i[0];
-	ALUOp_o <= ctrl_EX_i[2:1];
-	ALUSrc_o <= ctrl_EX_i[3];
-end
-
+	if(stall_i) begin
+		//stall
+	end
+	else begin
+		instr1115_o <= instr1115_i;
+		instr1620_MUX_o <= instr1620_MUX_i;
+		instr1620_FW_o <= instr1620_FW_i;
+		instr2125_o <= instr2125_i;
+		sign_extend_o <= sign_extend_i;
+		RS_data_o <= RS_data_i;
+		RT_data_o <= RT_data_i;
+		ctrl_WB_o <= ctrl_WB_i;
+		ctrl_M_o <= ctrl_M_i;
+		RegDst_o <= ctrl_EX_i[0];
+		ALUOp_o <= ctrl_EX_i[2:1];
+		ALUSrc_o <= ctrl_EX_i[3];
+	end
+end	
 endmodule
--- dcache_one_way_example/IF_ID.v
+++ dcache_one_way_example/IF_ID.v
@@ -5,13 +5,14 @@ module IF_ID
 	instr_i,
 	IFIDWrite_i,
 	flush_i,
+	stall_i,
 	addr_o,
 	instr_o
 );
 
 input			clk_i;
 input		[31:0]	addr_i, instr_i;
-input			IFIDWrite_i, flush_i;
+input			IFIDWrite_i, flush_i, stall_i;
 output reg	[31:0]	addr_o, instr_o;
 
 always@(negedge clk_i) begin
@@ -19,12 +20,12 @@ always@(negedge clk_i) begin
 		addr_o <= addr_i;
 		instr_o <= 32'b0;
 	end
+	else if(stall_i || IFIDWrite_i) begin
+		//stall
+	end
 	else if(IFIDWrite_i == 0) begin
 		addr_o <= addr_i;
 		instr_o <= instr_i;
-	end
-	else begin
-		//stall
 	end
 end
 
